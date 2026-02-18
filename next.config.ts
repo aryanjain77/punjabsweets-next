10e48ea839f5f3561959220ca30713048bc7bb3f
aryanjain77 <141511109+aryanjain77@users.noreply.github.com>
2026-02-18 16:44:51 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "localhost",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
