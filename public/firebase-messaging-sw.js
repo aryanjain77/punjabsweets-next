5ac0622377cc9e70defbbdb2b67ec7c8db219190
aryanjain77 <141511109+aryanjain77@users.noreply.github.com>
2026-06-26 20:24:47 +0530

    Update Firebase configuration in service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,12 +7,12 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyBcVFnxcHa-nq77y9jHmgYsdjDA9c0OxNY",
+  authDomain: "punjab-sweets-d93cf.firebaseapp.com",
+  projectId: "punjab-sweets-d93cf",
+  storageBucket: "punjab-sweets-d93cf.firebasestorage.app",
+  messagingSenderId: "966776137345",
+  appId: "1:966776137345:web:f9620a476e899b7d848f80"
 });
 
 const messaging = firebase.messaging();
